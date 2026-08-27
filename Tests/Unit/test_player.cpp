677bb68ef345f0a371f9a0610a677d0ff75b84d8
Rafael Bernardo <player1rgb@gmail.com>
2026-08-27 22:22:54 +0100

    test(9.6): migrate player tests to TickInput

--- Tests/Unit/test_player.cpp
+++ Tests/Unit/test_player.cpp
@@ -14,29 +14,24 @@ using namespace logic;
 TEST_SUITE("Player / Movimento e Salto") {
 
     TEST_CASE("Controlo Horizontal no chao") {
-        Player p; PhysicsWorld w; InputManager i;
+        Player p; PhysicsWorld w;
         p.body.isGrounded = true;
 
-        i.injectRawState(false, true, false, false, false);
-        p.update(i, w, PhysicsWorld::FIXED_STEP);
+        p.update(TickInput{false, true, false, false, false}, w, PhysicsWorld::FIXED_STEP);
         CHECK(p.velocity().x == doctest::Approx(config::PLAYER_MOVE_SPEED));
         CHECK(p.facingDirection == 1.0f);
 
-        i.injectRawState(true, false, false, false, false);
-        p.update(i, w, PhysicsWorld::FIXED_STEP);
+        p.update(TickInput{true, false, false, false, false}, w, PhysicsWorld::FIXED_STEP);
         CHECK(p.velocity().x == doctest::Approx(-config::PLAYER_MOVE_SPEED));
         CHECK(p.facingDirection == -1.0f);
     }
 
     TEST_CASE("Salto Parabolico e Perda de Controlo Aereo") {
-        Player p; PhysicsWorld w; InputManager i;
+        Player p; PhysicsWorld w;
         p.body.isGrounded = true;
 
-        i.injectRawState(false, true, false, false, false);
-        p.update(i, w, PhysicsWorld::FIXED_STEP);
-
-        i.injectRawState(false, false, false, true, true);
-        p.update(i, w, PhysicsWorld::FIXED_STEP);
+        p.update(TickInput{false, true, false, false, false}, w, PhysicsWorld::FIXED_STEP);
+        p.update(TickInput{false, false, false, true, true}, w, PhysicsWorld::FIXED_STEP);
 
         CHECK(p.isGrounded() == false);
 
@@ -47,8 +42,7 @@ TEST_SUITE("Player / Movimento e Salto") {
         CHECK(p.velocity().y == doctest::Approx(expectedY));
         CHECK(p.velocity().x == doctest::Approx(expectedX));
 
-        i.injectRawState(true, false, false, false, false);
-        p.update(i, w, PhysicsWorld::FIXED_STEP);
+        p.update(TickInput{true, false, false, false, false}, w, PhysicsWorld::FIXED_STEP);
         CHECK(p.velocity().x == doctest::Approx(expectedX));
     }
 
@@ -60,7 +54,8 @@ TEST_SUITE("Player / Movimento e Salto") {
         p.body.isGrounded = true;
         i.onKeyEvent(Key::C, Action::PRESS);
 
-        p.update(i, w, PhysicsWorld::FIXED_STEP);
+        const TickInput tick = i.tickInput(bindings, 0);
+        p.update(tick, w, PhysicsWorld::FIXED_STEP);
 
         CHECK(p.velocity().x == doctest::Approx(config::PLAYER_MOVE_SPEED));
         CHECK(p.facingDirection == 1.0f);
@@ -70,18 +65,16 @@ TEST_SUITE("Player / Movimento e Salto") {
 TEST_SUITE("Player / Queda de Borda") {
 
     TEST_CASE("cair da borda sem saltar: mantem inercia X a 100% (fisica normal)") {
-        Player p; PhysicsWorld w; InputManager i;
+        Player p; PhysicsWorld w;
 
         p.body.isGrounded = true;
-        i.injectRawState(false, true, false, false, false);
-        p.update(i, w, PhysicsWorld::FIXED_STEP);
+        p.update(TickInput{false, true, false, false, false}, w, PhysicsWorld::FIXED_STEP);
         REQUIRE(p.velocity().x == doctest::Approx(config::PLAYER_MOVE_SPEED));
 
         p.body.isGrounded = false;
         p.body.position.y = 100.0f;
 
-        i.injectRawState(false, false, false, false, false);
-        p.update(i, w, PhysicsWorld::FIXED_STEP);
+        p.update(TickInput{false, false, false, false, false}, w, PhysicsWorld::FIXED_STEP);
 
         CHECK(p.velocity().x == doctest::Approx(config::PLAYER_MOVE_SPEED));
     }
