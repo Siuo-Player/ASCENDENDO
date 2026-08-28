aaa40dd0bf0f460fccbd8a717443a10420e21d34
Rafael Bernardo <player1rgb@gmail.com>
2026-08-28 18:25:51 +0100

    refactor: move sprite GPU image ownership to shared primitive

--- Game/Graphics/SpriteRendererGpu.h
+++ Game/Graphics/SpriteRendererGpu.h
@@ -1,5 +1,7 @@
 #pragma once
 
+#include "Graphics/VulkanImageUpload.h"
+
 #include <vulkan/vulkan.h>
 
 #include <cstdint>
@@ -29,17 +31,10 @@ public:
     bool isInitialized() const { return m_initialized; }
 
 private:
-    uint32_t findMemoryType(uint32_t typeFilter, VkMemoryPropertyFlags props) const;
-    bool createTextureImage(const std::vector<unsigned char>& pixels,
-                            uint32_t width,
-                            uint32_t height);
     bool createDescriptorSet(VkDescriptorSetLayout layout);
 
     VulkanContext* m_ctx = nullptr;
-    VkImage m_image = VK_NULL_HANDLE;
-    VkDeviceMemory m_imageMemory = VK_NULL_HANDLE;
-    VkImageView m_imageView = VK_NULL_HANDLE;
-    VkSampler m_sampler = VK_NULL_HANDLE;
+    VulkanImageResource m_texture{};
     VkDescriptorPool m_descPool = VK_NULL_HANDLE;
     VkDescriptorSet m_descSet = VK_NULL_HANDLE;
     bool m_initialized = false;
