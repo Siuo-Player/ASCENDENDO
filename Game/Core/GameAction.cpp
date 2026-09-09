678658f13b9f49cef7c4bfc3cd09d4b7e5325ae9
Rafael Bernardo <player1rgb@gmail.com>
2026-09-09 23:35:04 +0100

    feat(21a): expose campaign editor action names

--- Game/Core/GameAction.cpp
+++ Game/Core/GameAction.cpp
@@ -8,36 +8,41 @@ namespace core {
 
 const char* actionDisplayName(GameAction action) {
     switch (action) {
-        case GameAction::MoveLeft:            return "Mover Esquerda";
-        case GameAction::MoveRight:           return "Mover Direita";
-        case GameAction::Jump:                return "Saltar";
-        case GameAction::Pause:               return "Pausa";
-        case GameAction::UIConfirm:           return "Confirmar";
-        case GameAction::UILeft:              return "Menu: Anterior";
-        case GameAction::UIRight:             return "Menu: Seguinte";
-        case GameAction::OpenEditor:          return "Abrir Editor de Nivel";
-        case GameAction::OpenCampaignEditor:  return "Abrir Editor de Campanha";
-        case GameAction::OpenControls:        return "Ver Controlos";
-        case GameAction::Quit:                return "Sair do Jogo";
-        case GameAction::EditorPanUp:         return "Editor: Deslocar Cima";
-        case GameAction::EditorPanDown:       return "Editor: Deslocar Baixo";
-        case GameAction::EditorCursorLeft:    return "Editor: Cursor Esquerda";
-        case GameAction::EditorCursorRight:   return "Editor: Cursor Direita";
-        case GameAction::EditorCursorUp:      return "Editor: Cursor Cima";
-        case GameAction::EditorCursorDown:    return "Editor: Cursor Baixo";
-        case GameAction::EditorToggleMode:    return "Editor: Alternar STAMP/DRAG";
-        case GameAction::EditorSizeDown:      return "Editor: Tamanho Menor";
-        case GameAction::EditorSizeUp:        return "Editor: Tamanho Maior";
-        case GameAction::EditorPlace:         return "Editor: Colocar";
-        case GameAction::EditorUndo:          return "Editor: Desfazer";
-        case GameAction::EditorRedo:          return "Editor: Refazer";
-        case GameAction::DeleteSelection:     return "Editor: Apagar Seleção";
-        case GameAction::EditorSave:          return "Editor: Guardar";
-        case GameAction::EditorTest:          return "Editor: Testar Nivel";
-        case GameAction::EditorValidate:      return "Editor: Validar Nivel";
-        case GameAction::EditorSelectPlatform:return "Editor: Selecionar Plataforma";
-        case GameAction::EditorSelectSpawn:   return "Editor: Selecionar Spawn";
-        case GameAction::EditorSelectFlag:    return "Editor: Selecionar Flag";
+        case GameAction::MoveLeft:             return "Mover Esquerda";
+        case GameAction::MoveRight:            return "Mover Direita";
+        case GameAction::Jump:                 return "Saltar";
+        case GameAction::Pause:                return "Pausa";
+        case GameAction::UIConfirm:            return "Confirmar";
+        case GameAction::UILeft:               return "Menu: Anterior";
+        case GameAction::UIRight:              return "Menu: Seguinte";
+        case GameAction::OpenEditor:           return "Abrir Editor de Nivel";
+        case GameAction::OpenCampaignEditor:   return "Abrir Editor de Campanha";
+        case GameAction::OpenControls:         return "Ver Controlos";
+        case GameAction::Quit:                 return "Sair do Jogo";
+        case GameAction::EditorPanUp:          return "Editor: Deslocar Cima";
+        case GameAction::EditorPanDown:        return "Editor: Deslocar Baixo";
+        case GameAction::EditorCursorLeft:     return "Editor: Cursor Esquerda";
+        case GameAction::EditorCursorRight:    return "Editor: Cursor Direita";
+        case GameAction::EditorCursorUp:       return "Editor: Cursor Cima";
+        case GameAction::EditorCursorDown:     return "Editor: Cursor Baixo";
+        case GameAction::EditorToggleMode:     return "Editor: Alternar STAMP/DRAG";
+        case GameAction::EditorSizeDown:       return "Editor: Tamanho Menor";
+        case GameAction::EditorSizeUp:         return "Editor: Tamanho Maior";
+        case GameAction::EditorPlace:          return "Editor: Colocar";
+        case GameAction::EditorUndo:           return "Editor: Desfazer";
+        case GameAction::EditorRedo:           return "Editor: Refazer";
+        case GameAction::DeleteSelection:      return "Editor: Apagar Seleção";
+        case GameAction::EditorSave:           return "Editor: Guardar";
+        case GameAction::EditorTest:           return "Editor: Testar Nivel";
+        case GameAction::EditorValidate:       return "Editor: Validar Nivel";
+        case GameAction::EditorSelectPlatform: return "Editor: Selecionar Plataforma";
+        case GameAction::EditorSelectSpawn:    return "Editor: Selecionar Spawn";
+        case GameAction::EditorSelectFlag:     return "Editor: Selecionar Flag";
+        case GameAction::CampaignSelectPrevious:return "Campanha: Nivel Anterior";
+        case GameAction::CampaignSelectNext:   return "Campanha: Nivel Seguinte";
+        case GameAction::CampaignMoveEarlier:   return "Campanha: Mover Nivel para Tras";
+        case GameAction::CampaignMoveLater:     return "Campanha: Mover Nivel para Frente";
+        case GameAction::CampaignOpenLevel:     return "Campanha: Abrir Nivel";
     }
     return "?";
 }
@@ -74,6 +79,11 @@ const char* actionSerializedName(GameAction action) {
         case GameAction::EditorSave:           return "EditorSave";
         case GameAction::EditorTest:           return "EditorTest";
         case GameAction::EditorValidate:       return "EditorValidate";
+        case GameAction::CampaignSelectPrevious:return "CampaignSelectPrevious";
+        case GameAction::CampaignSelectNext:   return "CampaignSelectNext";
+        case GameAction::CampaignMoveEarlier:  return "CampaignMoveEarlier";
+        case GameAction::CampaignMoveLater:    return "CampaignMoveLater";
+        case GameAction::CampaignOpenLevel:    return "CampaignOpenLevel";
     }
     return "Unknown";
 }
