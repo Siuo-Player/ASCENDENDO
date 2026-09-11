ee5a255416de84ad98181f32506f0de35e637391
Rafael Bernardo <player1rgb@gmail.com>
2026-09-11 16:09:09 +0100

    test(validation): use unambiguously unreachable vertical route

--- Tests/Python/test_campaign_validation.py
+++ Tests/Python/test_campaign_validation.py
@@ -63,7 +63,7 @@ class CampaignValidationTests(unittest.TestCase):
 
     def test_unreachable_level_is_rejected(self):
         path = self.write_level(
-            "NAME impossible\nPLATFORM 0 0 640 16\nPLATFORM 608 300 32 16\n"
+            "NAME impossible\nSCREENS 2\nPLATFORM 0 0 640 16\nPLATFORM 608 700 32 16\n"
         )
         report = module.validate_level(str(path))
         self.assertFalse(report["valid"])
