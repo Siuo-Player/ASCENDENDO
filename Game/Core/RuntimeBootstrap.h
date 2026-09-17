524d63488cae788f7cdba8cdb4d23745f10022a7
Rafael Bernardo <player1rgb@gmail.com>
2026-09-17 21:26:32 +0100

    feat(content): expose catalogued campaigns at bootstrap

--- Game/Core/RuntimeBootstrap.h
+++ Game/Core/RuntimeBootstrap.h
@@ -1,5 +1,6 @@
 #pragma once
 
+#include "Core/CampaignCatalogue.h"
 #include "Core/RuntimePaths.h"
 
 #include <filesystem>
@@ -11,6 +12,8 @@ namespace core {
 struct RuntimeBootstrapResult {
     RuntimePaths paths;
     bool userDirectoriesReady = false;
+    std::vector<CampaignDescriptor> campaigns;
+    std::size_t activeCampaignIndex = 0;
     std::vector<std::filesystem::path> campaign;
     std::string campaignID;
 
